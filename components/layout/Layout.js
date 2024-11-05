71d88c56ddf2d0e9beb1a3f962898d0a29abb2a5
Maryam Mirafzal/ <maryammirafzal9@gmail.com>
2024-11-05 10:14:00 +0330

    [ADD] Add Layout

---
+++ components/layout/Layout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from "./Layout.module.css"
+import Link from 'next/link'
+
+function Layout({children}) {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.left}>
+            <Link href="/">OrderFood</Link>
+        </div>
+        <div className={styles.right}>
+            <Link href="/menu">Menu</Link>
+            <Link href="/categories">Categories</Link>
+        </div>
+      </header>
+      <div className={styles.container}>{children}</div>
+      <footer className={styles.footer}>
+        Next.js Course | OrderFood Project &copy;
+      </footer>
+    </>
+  )
+}
+
+export default Layout
